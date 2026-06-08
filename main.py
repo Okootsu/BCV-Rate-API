1bf94fcf1f5cde75c594a256cb40e6d876a82249
Okootsu <163650919+Okootsu@users.noreply.github.com>
2026-06-07 20:15:37 -0400

    Add new origins for CORS in FastAPI app

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from fastapi.middleware.cors import CORSMiddleware
 origenes = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://localhost:8081",
+    "http://127.0.0.1:8081",
 ]
 
 app = FastAPI()
